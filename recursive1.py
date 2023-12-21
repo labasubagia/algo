d1380e12545d28d44202a8eef55c4d198ddad019
Laba Subagia <labasubagia22@gmail.com>
2023-12-22 00:14:28 +0800

    chore: example recursive 1

---
+++ recursive1.py
@@ -0,0 +1,73 @@
+"""
+recursive 1
+get direct & indirect related products
+"""
+
+import unittest
+from typing import List, Set
+
+
+class TestCase(unittest.TestCase):
+    def test_related_1(self):
+        products = [
+            ["AB", "A"],
+            ["AB", "B"],
+            ["BC", "B"],
+            ["BC", "C"],
+            ["DE", "D"],
+            ["DE", "E"],
+        ]
+        result1 = sorted(get_related_product_recursive(products, "AB"))
+        self.assertEqual(result1, sorted(set(["AB", "A", "B", "BC", "B", "C"])))
+
+        result2 = sorted(get_related_product_recursive(products, "E"))
+        self.assertEqual(result2, sorted(set(["DE", "D", "E"])))
+
+    def test_related_2(self):
+        products = [
+            ["AB", "A"],
+            ["AB", "B"],
+            ["CD", "C"],
+            ["CD", "D"],
+            ["EF", "E"],
+            ["EF", "F"],
+        ]
+        result1 = sorted(get_related_product_recursive(products, "B"))
+        self.assertEqual(result1, sorted(set(["AB", "A", "B"])))
+
+        result2 = sorted(get_related_product_recursive(products, "CD"))
+        self.assertEqual(result2, sorted(set(["CD", "C", "D"])))
+
+        result3 = sorted(get_related_product_recursive(products, "F"))
+        self.assertEqual(result3, sorted(set(["EF", "E", "F"])))
+
+
+def get_related_product_recursive(
+    products: List[List[str]], target_product: str, visited: Set[str] | None = None
+) -> Set[str]:
+    """
+    get_related_data_recursive
+    this fn is for get all related product [[bundle, component],...]
+    find the direct and indirect relation to a product
+    """
+
+    if visited is None:
+        visited = set()
+
+    related_data: Set[str] = set()
+
+    for bundle, component in products:
+        if bundle == target_product and component not in visited:
+            visited.add(component)
+            related_data.add(component)
+            related_data |= get_related_product_recursive(products, component, visited)
+        elif component == target_product and bundle not in visited:
+            visited.add(bundle)
+            related_data.add(bundle)
+            related_data |= get_related_product_recursive(products, bundle, visited)
+
+    return related_data
+
+
+if __name__ == "__main__":
+    unittest.main()
