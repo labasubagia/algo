b14b527e46b3287cfc84a173f55a2b017beddcf8
Laba Subagia <labasubagia22@gmail.com>
2023-12-25 17:34:50 +0800

    chore: recursive1 improve tail recursive and struct set

--- recursive1.go
+++ recursive1.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sort"
+	"unicode"
 )
 
 type Bundle struct {
@@ -12,9 +13,9 @@ type Bundle struct {
 
 func main() {
 	bundles := []Bundle{}
-	for r := 'A'; r < 'Z'; r++ {
-		p := fmt.Sprintf("%c", r)
-		n := fmt.Sprintf("%c", r+1)
+	for r := 'a'; r < 'z'; r++ {
+		p := fmt.Sprintf("%c", unicode.ToUpper(r))
+		n := fmt.Sprintf("%c", unicode.ToUpper(r+1))
 		bundles = append(bundles, Bundle{
 			SKU:      fmt.Sprintf("%v%v", p, n),
 			ChildSKU: p,
@@ -24,25 +25,32 @@ func main() {
 			ChildSKU: n,
 		})
 	}
-	fmt.Printf("%+v\n", bundles)
 
-	related := findRelated(bundles, []string{"A", "B", "C", "D"})
-	listRelated := []string{}
-	for SKU := range related {
-		listRelated = append(listRelated, SKU)
-	}
-	sort.Strings(listRelated)
-	fmt.Println(listRelated)
+	bundles = append(bundles, Bundle{
+		SKU:      "1_2",
+		ChildSKU: "1",
+	})
+
+	bundles = append(bundles, Bundle{
+		SKU:      "1_2",
+		ChildSKU: "2",
+	})
+
+	fmt.Printf("%v\n", bundles)
+
+	related := findRelated(bundles, []string{"A", "B", "XY", "1_2"})
+	sort.Strings(related)
+	fmt.Println(related)
 }
 
-func findRelated(bundles []Bundle, SKUs []string) map[string]bool {
-	mapResult := map[string]map[string]bool{}
+func findRelated(bundles []Bundle, SKUs []string) []string {
+	resultMap := map[string]map[string]struct{}{}
 	for _, SKU := range SKUs {
-		result, ok := mapResult[SKU]
+		result, ok := resultMap[SKU]
 		if !ok {
 			result = findRelatedPerSKU(bundles, SKU, nil)
 			for SKU := range result {
-				mapResult[SKU] = result
+				resultMap[SKU] = result
 			}
 			fmt.Printf("%v new\n", SKU)
 		} else {
@@ -50,36 +58,33 @@ func findRelated(bundles []Bundle, SKUs []string) map[string]bool {
 		}
 	}
 
-	result := map[string]bool{}
-	for _, item := range mapResult {
+	agg := map[string]struct{}{}
+	for _, item := range resultMap {
 		for SKU := range item {
-			result[SKU] = true
+			agg[SKU] = struct{}{}
 		}
 	}
-	return result
-}
 
-func findRelatedPerSKU(bundles []Bundle, target string, visited map[string]bool) map[string]bool {
-	if visited == nil {
-		visited = map[string]bool{}
-	}
-
-	related := map[string]bool{}
-	for _, bundle := range bundles {
-		var nextRelated map[string]bool
-		if bundle.SKU == target && !visited[bundle.ChildSKU] {
-			visited[bundle.ChildSKU] = true
-			related[bundle.ChildSKU] = true
-			nextRelated = findRelatedPerSKU(bundles, bundle.ChildSKU, visited)
-		} else if bundle.ChildSKU == target && !visited[bundle.SKU] {
-			visited[bundle.SKU] = true
-			related[bundle.SKU] = true
-			nextRelated = findRelatedPerSKU(bundles, bundle.SKU, visited)
-		}
-		for SKU := range nextRelated {
-			related[SKU] = true
-		}
-
+	related := []string{}
+	for SKU := range agg {
+		related = append(related, SKU)
 	}
 	return related
+}
+
+func findRelatedPerSKU(bundles []Bundle, target string, acc map[string]struct{}) map[string]struct{} {
+	if acc == nil {
+		acc = map[string]struct{}{}
+	}
+	for _, bundle := range bundles {
+		if _, ok := acc[bundle.ChildSKU]; bundle.SKU == target && !ok {
+			acc[bundle.ChildSKU] = struct{}{}
+			acc = findRelatedPerSKU(bundles, bundle.ChildSKU, acc)
+		} else if _, ok := acc[bundle.SKU]; bundle.ChildSKU == target && !ok {
+			acc[bundle.SKU] = struct{}{}
+			acc = findRelatedPerSKU(bundles, bundle.SKU, acc)
+		}
+
+	}
+	return acc
 }
