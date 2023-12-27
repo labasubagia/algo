81f3831ebc791f50596094c8de2a96b0c9eec1d4
Laba Subagia <labasubagia22@gmail.com>
2023-12-27 22:17:51 +0800

    chore: bubble sort other impl

--- bubble_sort.py
+++ bubble_sort.py
@@ -8,15 +8,27 @@ class TestCase(unittest.TestCase):
         arr = actual.copy()
         random.shuffle(arr)
 
-        expected1 = bubble_sort1(arr)
-        self.assertEqual(actual, expected1)
+        self.assertEqual(actual, bubble_sort1(arr.copy()))
+        self.assertEqual(actual, bubble_sort2(arr.copy()))
 
 
 def bubble_sort1(arr: List[int]) -> List[int]:
     for i in range(len(arr)):
         for j in range(i, len(arr)):
             if arr[i] > arr[j]:
-                arr[i], arr[j] = arr[j], arr[i]
+                arr[i], arr[j] = arr[j], arr[i] 
+    return arr
+
+def bubble_sort2(arr: List[int]) -> List[int]:
+    unsorted_until_index = len(arr)-1
+    sorted = False
+    while not sorted:
+        sorted = True
+        for i in range(unsorted_until_index):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                sorted = False
+        unsorted_until_index -= 1
     return arr
 
 if __name__ == '__main__':
