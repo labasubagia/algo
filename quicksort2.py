0827ef27bbd7a42f7b1215bc330ee6e89a9e2f18
Laba Subagia <labasubagia22@gmail.com>
2024-01-21 05:24:32 +0800

    chore: update quicksort2

--- quicksort2.py
+++ quicksort2.py
@@ -45,15 +45,15 @@ def quicksort2(arr: list[int], low: int, high: int):
 
 def partition(arr: list[int], low: int, high: int) -> int:
     pivot = arr[high]
-    i = low - 1
+    i = low
 
     for j in range(low, high):
         if arr[j] <= pivot:
-            i += 1
             arr[i], arr[j] = arr[j], arr[i]
+            i += 1
 
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
+    arr[i], arr[high] = arr[high], arr[i]
+    return i
 
 
 if __name__ == "__main__":
